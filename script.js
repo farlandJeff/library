7c3a7253314ee8c9dd7914e1cb1104cf7f237f4e
Jeff <farlandjeff@gmail.com>
2022-12-16 19:08:05 -0500

    Add book object constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+
+}
