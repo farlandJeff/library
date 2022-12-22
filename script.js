b401ee17dd3d545c421078b983d5916c3a2b0adb
Jeff <farlandjeff@gmail.com>
2022-12-21 19:59:53 -0500

    Receive input from user

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector('.modal');
 const newBtn = document.querySelector('.new-btn');
 const closeBtn = document.querySelector('.close-form');
+const addBtn = document.querySelector('#add-btn');
+
 
 newBtn.addEventListener('click', () => {
     modal.classList.add("active");
@@ -10,11 +12,11 @@ closeBtn.addEventListener('click', () => {
     modal.classList.remove("active");
 });
 
-
+addBtn.addEventListener('click', getBook);
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -23,4 +25,15 @@ function Book() {
 
 function addBookToLibrary() {
     
+    //getBook();
+    //let book = new Book();
+}
+
+function getBook() {
+    event.preventDefault();
+    let tmpTitle = document.querySelector('#title').value;
+    let tmpAuther = document.querySelector('#author').value;
+    let tmpPages = document.querySelector('#pages').value;
+    let tmpRead = document.querySelector('#read').value;
+    console.log(tmpRead);
 }
